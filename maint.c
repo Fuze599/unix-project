8328f9111187fd8fa3a2d54e324440da7116dab1
François Bardijn <francois.bardijn@hotmail.com>
2022-05-04 14:47:59 +0200

    [FIX] concurrency problem

--- maint.c
+++ maint.c
@@ -17,15 +17,17 @@ int main(int argc, char **argv) {
 	sem_create(SHM_SEM_KEY, 1, PERM, 1);
 	sshmget(SHM_SEM_KEY, NB_CLIENT * sizeof(int), IPC_CREAT | PERM);
   } else if (type == 2) {
+  	int sem_id = sem_get(SHM_SEM_KEY, 1);
+  	sem_down0(sem_id);
   	int shm_id = sshmget(SHM_SEM_KEY, NB_CLIENT * sizeof(int), IPC_CREAT | PERM);
 	sshmdelete(shm_id);
-	int sem_id = sem_get(SHM_SEM_KEY, 1);
   	sem_delete(sem_id);
   } else if (type == 3) {
   	if (argc >= 3) {
   		int sem_id = sem_get(SHM_SEM_KEY, 1);
   		int opt = atoi(argv[2]);
   		sem_down0(sem_id);
+  		printf("Mémoire réservée pour %d secondes\n", opt);
   		sleep(opt);
   		sem_up0(sem_id);
   	} else {
