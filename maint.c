b5a094f0c17fb80bc6ac2b817f59ca29b729833b
François Bardijn <francois.bardijn@hotmail.com>
2022-05-04 14:28:06 +0200

    [FIX] semaphore and shm delete

--- maint.c
+++ maint.c
@@ -12,20 +12,18 @@ int main(int argc, char **argv) {
   }
 
   int type = atoi(argv[1]);
+  if (type == 1) {
+	sem_create(SHM_SEM_KEY, 1, PERM, 1);
+	sshmget(SHM_SEM_KEY, NB_CLIENT * sizeof(int), IPC_CREAT | PERM);
+  } else if (type == 2) {
+  	int shm_id = sshmget(SHM_SEM_KEY, NB_CLIENT * sizeof(int), IPC_CREAT | PERM);
+	sshmdelete(shm_id);
+	int sem_id = sem_get(SHM_SEM_KEY, 1);
+  	sem_delete(sem_id);
+  } else if (type == 3) {
 
-  switch(type) {
-  	case 1:
-	  	int listAccount[NB_CLIENT];
-	  	sem_create(SHM_SEM_KEY, 1, PERM, 1);
-	  	sshmget(SHM_SEM_KEY, sizeof(listAccount), IPC_CREAT | PERM);
-  		break;
-  	case 2:
-  		sshmdelete(SHM_SEM_KEY);
-  		sem_delete(SHM_SEM_KEY);
-  		break;
-  	case 3:
-  		break;
-  	default:
-  		exit(1);
+  } else {
+  	perror("Bad argument");
+   	exit(1);
   }
 }
